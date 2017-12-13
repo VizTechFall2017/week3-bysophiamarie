8d7faca7e0365065c629442c895e96702772354a
bysophiamarie <32103427+bysophiamarie@users.noreply.github.com>
2017-12-13 15:37:47 -0500

    Commit

--- PS03/PS03/index.js
+++
@@ -1,7 +0,0 @@
-var svg = d3.select('svg');
-
-/* Your code goes here */
-
-d3.csv("PS03_FoodPrices.csv", function(global){
-    console.log(global)
-});
--- PS03/index.js
+++ PS03/index.js
@@ -2,5 +2,6 @@ var svg = d3.select('svg');
 
 /* Your code goes here */
 
-
-
+d3.csv("PS03_GOT_houses_final2.csv", function(global){
+    console.log(global)
+});
